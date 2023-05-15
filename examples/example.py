c7cd35a69f3988b283a26abed5d2340f261070b4
Joakim Nilsson <94443280+Joakim-Nilsson@users.noreply.github.com>
2023-05-15 09:31:51 +0200

    pyshacl edited path

--- examples/example.py
+++ examples/example.py
@@ -3,15 +3,20 @@ from pyshacl import validate
 from os import path
 import json
 
-data_ttl_file = \
-    filepath = "C:\\Users\\joanilss\\FK\\feldata.ttl"
+shacl_file = \
+    filepath = "path/to/your/shape/file.ttl"
 
+data_graph = \
+    filepath = "C:\\Users\\joanilss\\FK\\instantiated_data.ttl"
 
-data_ttl_file = path.abspath(data_ttl_file) 
+shacl_file = path.abspath(shacl_file)
+data_graph = path.abspath(data_graph) 
 
+# Run the validation
+conforms, v_graph, v_text = validate(data_graph, shacl_graph=shacl_file, ont_graph=None, inference='none', abort_on_error=False, meta_shacl=False, debug=False, js=False, advanced=False)
 
-conforms, v_graph, v_text = validate(data_ttl_file, shacl_graph=None, inference='rdfs',
-                                     serialize_report_graph=True)
+#conforms, v_graph, v_text = validate(data_ttl_file, shacl_graph=None, inference='rdfs',
+                                    # serialize_report_graph=True)
 
 result_json = json.dumps({
     "conforms": conforms,
@@ -19,8 +24,14 @@ result_json = json.dumps({
     "validation_text": v_text
 }, indent=4)
 
+if conforms:
+    print("Data conforms to SHACL shapes")
+else:
+    print(v_text)
+
 print(json.dumps(result_json, indent=4))
 print(result_json)
+
 
 print(conforms)
 print(v_graph)
